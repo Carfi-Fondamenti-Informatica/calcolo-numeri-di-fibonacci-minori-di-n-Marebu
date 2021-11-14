98e68f82c4d3a04e45e918f3d1b02c42c0a72eb2
Marebu <91844335+Marebu@users.noreply.github.com>
2021-11-14 17:18:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
+   int n=0;
+   cin>>n;
+   int a=1,b=0,c=0;
+   while(c<=n){
+      c=a+b;
+      a=b;
+      b=c;
+      cout<<c<<endl;
+      c=a+b;
+   }
    return 0;
 }
